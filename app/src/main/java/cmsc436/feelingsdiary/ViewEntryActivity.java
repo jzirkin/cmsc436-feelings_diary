4a5a43dab7c5a1902f409d901385d80e67adb252
Josh Zirkin <joshuaz@me.com>
2018-11-09 17:09:01 -0500

    Began creating layout

---
+++ app/src/main/java/cmsc436/feelingsdiary/ViewEntryActivity.java
@@ -0,0 +1,13 @@
+package cmsc436.feelingsdiary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_entry);
+    }
+}
